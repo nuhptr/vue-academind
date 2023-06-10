ee95465a8a7795008a43e77c7bedbf7815a1d4d2
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-06-10 23:16:40 +0700

    vue coms assignment and potential problem

---
+++ vue-communication-assignment/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import UserData from "./components/UserData.vue";
+import ActiveUser from "./components/ActiveUser.vue";
+
+export default {
+   components: {
+      UserData,
+      ActiveUser,
+   },
+   data() {
+      return {
+         user: {
+            name: "Maximilian",
+            age: 27,
+         },
+      };
+   },
+   methods: {
+      setUserData(name, age) {
+         this.user.name = name;
+         this.user.age = +age;
+      },
+   },
+};
+</script>
+
+<template>
+   <div class="app">
+      <ActiveUser :username="user.name" :userage="user.age" />
+      <UserData @set-data="setUserData" />
+   </div>
+</template>
+
+<style></style>
