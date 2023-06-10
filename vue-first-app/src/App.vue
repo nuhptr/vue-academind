fd323a2e7abb8a584275a2fa3d64a276230ce94a
nuhptr <nugrahaadi733@gmail.com>
2023-06-10 16:15:13 +0700

    adding component & connecting them

--- vue-first-app/src/App.vue
+++ vue-first-app/src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import FriendContact from "./components/FriendContact.vue";
+import NewFriend from "./components/NewFriend.vue";
 
 export default {
    data() {
@@ -24,6 +25,7 @@ export default {
    },
    components: {
       FriendContact,
+      NewFriend,
    },
    methods: {
       toggleFavoriteStatus(friendId) {
@@ -41,6 +43,7 @@ export default {
       <header>
          <h1>My Friend</h1>
       </header>
+      <NewFriend />
       <ul>
          <FriendContact
             v-for="friend in friends"
@@ -88,7 +91,8 @@ header {
    list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
    margin: 1rem auto;
    border-radius: 10px;
---
+++ vue-first-app/src/components/NewFriend.vue
@@ -0,0 +1,48 @@
+<template>
+   <form action="#">
+      <div>
+         <label for="name">Name</label>
+         <input type="text" name="name" />
+      </div>
+      <div>
+         <label for="phone">Phone</label>
+         <input type="tel" name="phone" />
+      </div>
+      <div>
+         <label for="email">Email</label>
+         <input type="email" name="email" />
+      </div>
+      <div>
+         <button>Add Contact</button>
+      </div>
+   </form>
+</template>
+
+<script>
+export default {
+   emits: ["add-contact"],
+   data() {
+      return {
+         enteredName: "",
+         enteredPhone: "",
+         enteredEmail: "",
+      };
+   },
+};
+</script>
+
+<style>
+#app input {
+   font: inherit;
+   padding: 0.15rem;
+}
+#app label {
+   font-weight: bold;
+   margin-right: 1rem;
+   width: 7rem;
+   display: inline-block;
+}
+#app form div {
+   margin: 1rem 0;
+}
+</style>
