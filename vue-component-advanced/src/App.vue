f8da59a3b3be9bdeca24faa9db4d04b3bdc0aeae
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-06-16 22:11:07 +0700

    component advanced

---
+++ vue-component-advanced/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+   <div>
+      <TheHeader />
+      <BadgeList />
+      <UserInfo
+         :full-name="activeUser.name"
+         :info-text="activeUser.description"
+         :role="activeUser.role" />
+   </div>
+</template>
+
+<script>
+import BadgeList from "./components/BadgeList.vue";
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+
+export default {
+   data() {
+      return {
+         activeUser: {
+            name: "Maximilian Schwarzmüller",
+            description: "Site owner and admin",
+            role: "admin",
+         },
+      };
+   },
+   components: { TheHeader, BadgeList, UserInfo },
+};
+</script>
+
+<style>
+html {
+   font-family: sans-serif;
+}
+
+body {
+   margin: 0;
+}
+</style>
---
+++ vue-component-advanced/src/components/UserInfo.vue
@@ -0,0 +1,32 @@
+<template>
+   <section>
+      <BaseCard>
+         <!-- named slot -->
+         <template v-slot:header>
+            <h3>{{ fullName }}</h3>
+            <BaseBadge :type="role" :caption="role.toUpperCase()" />
+         </template>
+         <template v-slot:default>
+            <p>{{ infoText }}</p>
+         </template>
+      </BaseCard>
+   </section>
+</template>
+
+<script>
+import BaseBadge from "./BaseBadge.vue";
+import BaseCard from "./BaseCard.vue";
+
+export default {
+   props: ["fullName", "infoText", "role"],
+   components: { BaseBadge, BaseCard },
+};
+</script>
+
+<style scoped>
+section header {
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+}
+</style>
