0cd9094cfaeea1359a0c7f7ee6406c0d1c76b6ae
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-06-16 09:50:58 +0700

    communication potential

---
+++ vue-communication-potential-problem/src/components/KnowledgeGrid.vue
@@ -0,0 +1,19 @@
+<template>
+   <ul>
+      <KnowledgeElement
+         v-for="topic in topics"
+         :key="topic.id"
+         :id="topic.id"
+         :topicName="topic.title"
+         :description="topic.description" />
+   </ul>
+</template>
+
+<script>
+import KnowledgeElement from "./KnowledgeElement.vue";
+
+export default {
+   inject: ["topics"],
+   components: { KnowledgeElement },
+};
+</script>
