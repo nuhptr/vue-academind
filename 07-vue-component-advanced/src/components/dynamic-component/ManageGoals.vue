71e72cfd52f81eee04c7e804245b9395f2a37e2f
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-08-07 17:12:17 +0700

    keep alive and teleport component

--- 07-vue-component-advanced/src/components/dynamic-component/ManageGoals.vue
+++ 07-vue-component-advanced/src/components/dynamic-component/ManageGoals.vue
@@ -1,8 +1,39 @@
 <template>
    <div>
       <h2>Manage Goals</h2>
-      <input type="text">
+      <input type="text" ref="goal" />
+      <button @click="setGoal">Set Goal</button>
+      <!-- teleport component -->
+      <teleport to="body">
+         <ErrorAlert v-if="inputIsInvalid">
+            <h2>Input is invalid</h2>
+            <p>Please enter at least a few characters!</p>
+            <button @click="confirmError">Okay</button>
+         </ErrorAlert>
+      </teleport>
    </div>
 </template>
 
-<script></script>
+<script>
+   import ErrorAlert from './ErrorAlert.vue';
+
+   export default {
+      data() {
+         return {
+            inputIsInvalid: false,
+         };
+      },
+      methods: {
+         setGoal() {
+            const enteredValue = this.$refs.goal.value;
+            if (enteredValue === '') {
+               this.inputIsInvalid = true;
+            }
+         },
+         confirmError() {
+            this.inputIsInvalid = false;
+         },
+      },
+      components: { ErrorAlert },
+   };
+</script>
