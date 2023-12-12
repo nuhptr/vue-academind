7cfd4052a25c5438a6521efff5400711762c367a
nuhptr <nugrahaadi733@gmail.com>
2023-12-12 16:28:25 +0700

    component-community/update code

--- 08-component-communications/src/App.vue
+++ 08-component-communications/src/App.vue
@@ -1,47 +1,99 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+   import FriendContact from "./components/FriendContact.vue"
+
+   export default {
+      components: { FriendContact },
+      data() {
+         return {
+            friends: [
+               {
+                  id: "manuel",
+                  name: "Manuel Lorenz",
+                  phone: "0123 45678 90",
+                  email: "manuel@localhost.com",
+               },
+               {
+                  id: "julie",
+                  name: "Julie Jones",
+                  phone: "0987 654421 21",
+                  email: "julie@localhost.com",
+               },
+            ],
+         }
+      },
+   }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+   <section>
+      <header>
+         <h1>My Friends</h1>
+      </header>
+      <ul>
+         <FriendContact name="Manuel Lorenz" phoneNumber="0123 45678 90" emailAddress="manuel@localhost.com" />
+         <FriendContact name="JulieJonas" phoneNumber="0981 329 128" emailAddress="julie@localhost.com" />
+      </ul>
+   </section>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
+   * {
+      box-sizing: border-box;
+      margin: 0;
+   }
+   html {
+      font-family: "Jost", sans-serif;
+   }
+   header {
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+      margin: 3rem auto;
+      border-radius: 10px;
+      padding: 1rem;
+      background-color: #58004d;
+      color: white;
+      text-align: center;
+      width: 90%;
+      max-width: 40rem;
+   }
+   #app {
+      & ul {
+         margin: 0;
+         padding: 0;
+         list-style: none;
+      }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+      & li {
+         box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+         margin: 1rem auto;
+         border-radius: 10px;
+         padding: 1rem;
+         text-align: center;
+         width: 90%;
+         max-width: 40rem;
+      }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+      & h2 {
+         font-size: 2rem;
+         border-bottom: 4px solid #ccc;
+         color: #58004d;
+         margin: 0 0 1rem 0;
+      }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+      & button {
+         font: inherit;
+         cursor: pointer;
+         border: 1px solid #ff0077;
+         background-color: #ff0077;
+         color: white;
+         padding: 0.05rem 1rem;
+         box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+         &:hover,
+         &:active {
+            background-color: #ec3169;
+            border-color: #ec3169;
+            box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+         }
+      }
+   }
 </style>
