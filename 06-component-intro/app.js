1af67e18300f0aea95e8b543fef2e0165a2a1434
nuhptr <nugrahaadi733@gmail.com>
2023-12-12 12:46:57 +0700

    first-vue-app

--- 06-component-intro/app.js
+++ 06-component-intro/app.js
@@ -1,26 +1,26 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      friends: [
-        {
-          id: 'manuel',
-          name: 'Manuel Lorenz',
-          phone: '01234 5678 991',
-          email: 'manuel@localhost.com',
-        },
-        {
-          id: 'julie',
-          name: 'Julie Jones',
-          phone: '09876 543 221',
-          email: 'julie@localhost.com',
-        },
-      ],
-    };
-  },
-});
+   data() {
+      return {
+         friends: [
+            {
+               id: "manuel",
+               name: "Manuel Lorenz",
+               phone: "01234 5678 991",
+               email: "manuel@localhost.com",
+            },
+            {
+               id: "julie",
+               name: "Julie Jones",
+               phone: "09876 543 221",
+               email: "julie@localhost.com",
+            },
+         ],
+      }
+   },
+})
 
-app.component('friend-contact', {
-  template: `
+app.component("friend-contact", {
+   template: `
   <li>
     <h2>{{ friend.name }}</h2>
     <button @click="toggleDetails()">
@@ -32,22 +32,22 @@ app.component('friend-contact', {
     </ul>
   </li>
   `,
-  data() {
-    return {
-      detailsAreVisible: false,
-      friend: {
-        id: 'manuel',
-        name: 'Manuel Lorenzo',
-        phone: '01234 5678 991',
-        email: 'manuel@localhost.com',
+   data() {
+      return {
+         detailsAreVisible: false,
+         friend: {
+            id: "manuel",
+            name: "Manuel Lorenzo",
+            phone: "01234 5678 991",
+            email: "manuel@localhost.com",
+         },
+      }
+   },
+   methods: {
+      toggleDetails() {
+         this.detailsAreVisible = !this.detailsAreVisible
       },
-    };
-  },
-  methods: {
-    toggleDetails() {
-      this.detailsAreVisible = !this.detailsAreVisible;
-    },
-  },
-});
+   },
+})
 
-app.mount('#app');
+app.mount("#app")
