bc11ec1d50de5001659e28537c10d796bfa39756
nuhptr <nugrahaadi733@gmail.com>
2023-12-13 00:02:47 +0700

    potential-problem/add new project

---
+++ 08-potential-problem/src/components/ActiveElement.vue
@@ -0,0 +1,14 @@
+<script>
+   export default {
+      props: ["topicTitle", "text"],
+   }
+</script>
+
+<template>
+   <section>
+      <h2>{{ topicTitle }}</h2>
+      <p>{{ text }}</p>
+   </section>
+</template>
+
+<style></style>
