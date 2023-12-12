b4a49381f8df461a7ce7aeba9f62589cf716506b
nuhptr <nugrahaadi733@gmail.com>
2023-12-12 22:32:16 +0700

    component/update

--- 08-component-communications/src/components/FriendContact.vue
+++ 08-component-communications/src/components/FriendContact.vue
@@ -1,18 +1,30 @@
 <script>
    export default {
-      props: ["name", "phoneNumber", "emailAddress"],
+      // define the props that this component expects to receive
+      props: {
+         id: { type: String, required: true },
+         name: { type: String, required: true },
+         phoneNumber: { type: String, required: true },
+         emailAddress: { type: String, required: true },
+         isFavorite: {
+            type: Boolean,
+            required: false,
+            default: false,
+            // validator: (value) => value === "0" || value === "1",
+         },
+      },
+
+      //* define the events that this component emits
+      emits: ["toogleFavorite", "deleteFriend"],
       data() {
          return {
             detailsAreVisible: false,
-            friend: {
-               id: "manuel",
-               name: "Manuel Lorenz",
-               phone: "0123 45678 90",
-               email: "manuel@localhost.com",
-            },
          }
       },
       methods: {
+         toogleFavorite() {
+            this.$emit("toogleFavorite", this.id)
+         },
          toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible
          },
@@ -22,17 +34,13 @@
 
 <template>
    <li>
-      <h2>{{ name }}</h2>
+      <h2>{{ name }} {{ isFavorite ? "- (Favorite)" : "" }}</h2>
+      <button @click="toogleFavorite" style="margin-right: 20px">Is favorite</button>
       <button @click="toggleDetails">{{ detailsAreVisible ? "Hide" : "Show" }} Details</button>
       <ul v-if="detailsAreVisible">
-         <li>
-            <strong>Phone:</strong>
-            {{ phoneNumber }}
-         </li>
-         <li>
-            <strong>Email:</strong>
-            {{ emailAddress }}
-         </li>
+         <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+         <li><strong>Email:</strong> {{ emailAddress }}</li>
       </ul>
+      <button @click="$emit('deleteFriend', id)" style="margin-left: 20px">Delete</button>
    </li>
 </template>
---
+++ 08-component-communications/src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<script>
+   export default {
+      data() {
+         return {
+            enteredName: "",
+            enteredPhone: "",
+            enteredEmail: "",
+         }
+      },
+      emits: ["addContact"],
+      methods: {
+         submitData() {
+            const contactData = { name: this.enteredName, phone: this.enteredPhone, email: this.enteredEmail }
+            this.$emit("addContact", contactData)
+            this.enteredName = ""
+            this.enteredPhone = ""
+            this.enteredEmail = ""
+         },
+      },
+   }
+</script>
+
+<template>
+   <!-- * For submit and preventDefault() window screen -->
+   <form @submit.prevent="submitData">
+      <div>
+         <label for="name">Name</label>
+         <input type="text" v-model="enteredName" />
+      </div>
+      <div>
+         <label for="phone">Phone</label>
+         <input type="tel" v-model="enteredPhone" />
+      </div>
+      <div>
+         <label for="email">E-Mail</label>
+         <input type="email" v-model="enteredEmail" />
+      </div>
+      <button>Add Contact</button>
+   </form>
+</template>
+
+<style></style>
