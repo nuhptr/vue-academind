514d935ec9eb47110cecd2659981dba3872823b5
nuhptr <nugrahaadi733@gmail.com>
2023-12-13 19:21:45 +0700

    deep-component/style scoped

--- 09-deep-component/src/App.vue
+++ 09-deep-component/src/App.vue
@@ -1,47 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+   import TheHeader from "./components/TheHeader.vue"
+   import BadgeList from "./components/BadgeList.vue"
+   import UserInfo from "./components/UserInfo.vue"
+
+   export default {
+      components: { UserInfo, TheHeader, BadgeList, UserInfo },
+      data() {
+         return {
+            activeUser: {
+               name: "Maximilian Schwarzmüller",
+               description: "Site owner and admin",
+               role: "admin",
+            },
+         }
+      },
+   }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+   <div>
+      <TheHeader />
+      <BadgeList />
+      <UserInfo :fullName="activeUser.name" :infoText="activeUser.description" :role="activeUser.role" />
+   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
+   html {
+      font-family: sans-serif;
+   }
+   body {
+      margin: 0;
+   }
 </style>
---
+++ 09-deep-component/src/components/UserInfo.vue
@@ -0,0 +1,34 @@
+<script>
+   import BadgeBase from "./BadgeBase.vue"
+
+   export default {
+      components: { BadgeBase },
+      props: ["fullName", "infoText", "role"],
+   }
+</script>
+
+<template>
+   <section>
+      <div>
+         <h3>{{ fullName }}</h3>
+         <BadgeBase :type="role" :caption="role.toUpperCase()" />
+      </div>
+      <p>{{ infoText }}</p>
+   </section>
+</template>
+
+<style scoped>
+   section {
+      margin: 2rem auto;
+      max-width: 30rem;
+      border-radius: 12px;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+      padding: 1rem;
+
+      & div {
+         display: flex;
+         justify-content: space-between;
+         align-items: center;
+      }
+   }
+</style>
