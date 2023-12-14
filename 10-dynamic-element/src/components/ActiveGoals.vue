187537cb3bba829941393b2659c335b1f24d2ec5
nuhptr <nugrahaadi733@gmail.com>
2023-12-14 14:39:11 +0700

    dynamic-element/

---
+++ 10-dynamic-element/src/components/ActiveGoals.vue
@@ -0,0 +1,13 @@
+<script>
+   export default {
+      data() {
+         return {}
+      },
+   }
+</script>
+
+<template>
+   <h2>Active Goals</h2>
+</template>
+
+<style></style>
---
+++ 10-dynamic-element/src/components/ManageGoals.vue
@@ -0,0 +1,13 @@
+<script>
+   export default {
+      data() {
+         return {}
+      },
+   }
+</script>
+
+<template>
+   <h2>Manage Goals</h2>
+</template>
+
+<style></style>
