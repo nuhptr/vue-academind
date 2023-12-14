1abbf947dbd8449d59671884a637d7cbf0c58a25
nuhptr <nugrahaadi733@gmail.com>
2023-12-14 10:19:25 +0700

    ./app update

--- 09-deep-component/src/App.vue
+++ 09-deep-component/src/App.vue
@@ -1,10 +1,11 @@
 <script>
    import Header from "./components/Header.vue"
-   import BadgeList from "./components/BadgeList.vue"
    import UserInfo from "./components/UserInfo.vue"
+   import BadgeList from "./components/BadgeList.vue"
+   import CourseGoals from "./components/goals/CourseGoals.vue"
 
    export default {
-      components: { UserInfo, Header, BadgeList, UserInfo },
+      components: { UserInfo, Header, BadgeList, CourseGoals },
       data() {
          return {
             activeUser: {
@@ -22,6 +23,10 @@
       <Header />
       <BadgeList />
       <UserInfo :fullName="activeUser.name" :infoText="activeUser.description" :role="activeUser.role" />
+      <CourseGoals #default="slotProps">
+         <h2>{{ slotProps.item }}</h2>
+         <p>{{ slotProps.anotherProp }}</p>
+      </CourseGoals>
    </div>
 </template>
 
