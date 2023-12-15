4dc7ccec962fa3e7289ab0da1b9a62d824dce970
nuhptr <nugrahaadi733@gmail.com>
2023-12-15 10:25:53 +0700

    app/update code

--- 11-learning-resources-project/src/App.vue
+++ 11-learning-resources-project/src/App.vue
@@ -1,39 +1,15 @@
 <script>
-   import StoredResource from "./components/resource/StoredResource.vue"
    import Theheader from "./components/layout/TheHeader.vue"
+   import TheResources from "./components/resource/TheResources.vue"
 
    export default {
-      components: { StoredResource, Theheader },
-      data() {
-         return {
-            storedResources: [
-               {
-                  id: "vue-mastery",
-                  title: "Learn Vue",
-                  description: "Learn Vue in 30 minutes",
-                  link: "https://www.vuemastery.com/courses/intro-to-vue-js/vue-instance",
-               },
-               {
-                  id: "css-mastery",
-                  title: "Learn CSS",
-                  description: "Learn CSS in 30 minutes",
-                  link: "https://www.udemy.com/course/the-complete-css-mastery-course/",
-               },
-               {
-                  id: "html-mastery",
-                  title: "Learn HTML",
-                  description: "Learn HTML in 30 minutes",
-                  link: "https://www.udemy.com/course/html-and-css-basics-to-advance-with-real-website-designs/",
-               },
-            ],
-         }
-      },
+      components: { Theheader, TheResources },
    }
 </script>
 
 <template>
    <Theheader title="RememberMe" />
-   <StoredResource :key="storedResources.id" :resources="storedResources" />
+   <TheResources />
 </template>
 
 <style>
