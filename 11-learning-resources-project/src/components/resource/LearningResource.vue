4b5730912fa6e8657afd947242e7562707648ea4
nuhptr <nugrahaadi733@gmail.com>
2023-12-15 09:08:42 +0700

    component/update

---
+++ 11-learning-resources-project/src/components/resource/LearningResource.vue
@@ -0,0 +1,54 @@
+<script>
+   import BaseCard from "../ui/BaseCard.vue"
+
+   export default {
+      components: { BaseCard },
+      props: ["data"],
+      data() {
+         return {}
+      },
+   }
+</script>
+
+<template>
+   <li>
+      <BaseCard>
+         <header>
+            <h3>{{ data.title }}</h3>
+            <button>Delete</button>
+         </header>
+         <p>{{ data.description }}</p>
+         <nav>
+            <a :href="data.link" target="_blank" noopener noreferrer>View Resource</a>
+         </nav>
+      </BaseCard>
+   </li>
+</template>
+
+<style scoped>
+   li {
+      margin: auto;
+      max-width: 40rem;
+   }
+   header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+   }
+   h3 {
+      font-size: 1.25rem;
+      margin: 0.5rem 0;
+   }
+   p {
+      margin: 0.5rem 0;
+   }
+   a {
+      text-decoration: none;
+      color: #ce5c00;
+
+      &:hover,
+      &:active {
+         color: #c89300;
+      }
+   }
+</style>
---
+++ 11-learning-resources-project/src/components/resource/StoredResource.vue
@@ -0,0 +1,24 @@
+<script>
+   import LearningResource from "./LearningResource.vue"
+
+   export default {
+      components: { LearningResource },
+      props: ["resources"],
+   }
+</script>
+
+<template>
+   <ul>
+      <LearningResource v-for="response in resources" :key="response.id" :data="response" />
+   </ul>
+</template>
+
+<style>
+   ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      margin: auto;
+      max-width: 40rem;
+   }
+</style>
