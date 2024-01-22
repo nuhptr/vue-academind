a3e5b8608c583e4f453dfe2386ea176eeebea4e9
nuhptr <nugrahaadi733@gmail.com>
2024-01-22 17:36:07 +0700

    vuex:boilerplate

---
+++ 17-vuex/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+   import BaseContainer from "@/components/BaseContainer.vue"
+
+   export default {
+      components: { BaseContainer },
+   }
+</script>
+
+<template>
+   <BaseContainer title="Vuex"></BaseContainer>
+</template>
+
+<style>
+   * {
+      box-sizing: border-box;
+   }
+
+   html {
+      font-family: sans-serif;
+   }
+
+   body {
+      margin: 0;
+   }
+</style>
