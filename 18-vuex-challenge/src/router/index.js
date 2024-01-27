7f8ed6cc8c704d5f9d15769cce992a251029d8b9
nuhptr <nugrahaadi733@gmail.com>
2024-01-27 22:37:39 +0700

    vuex-challenge:boilerplate

---
+++ 18-vuex-challenge/src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+import ProductList from "../pages/ProductList.vue"
+import UserCart from "../pages/UserCart.vue"
+import ShopAdmin from "../pages/ShopAdmin.vue"
+
+const router = createRouter({
+   history: createWebHistory,
+   routes: [
+      { path: "/", redirect: "/products" },
+      { path: "/products", component: ProductList },
+      { path: "/cart", component: UserCart },
+      { path: "/admin", component: ShopAdmin },
+   ],
+})
+
+export default router
