ef9ef6c80d962cd9f61465751ddf37aa0bdc38d7
nuhptr <nugrahaadi733@gmail.com>
2024-02-04 15:44:56 +0700

    22-mixin-composition-1

---
+++ 22-mixin-composition-1/src/components/AddUser.vue
@@ -0,0 +1,24 @@
+<template>
+   <user-alert v-if="alertIsVisible" title="Add a User?" @close="hideAlert">
+      <p>Do you want to continue with adding a user?</p>
+   </user-alert>
+   <section>
+      <h2>Add a User</h2>
+      <button @click="showAlert">Add User</button>
+   </section>
+</template>
+
+<script setup>
+   import { ref } from "vue"
+
+   import UserAlert from "./UserAlert.vue"
+
+   const alertIsVisible = ref(false)
+
+   const showAlert = () => {
+      alertIsVisible.value = true
+   }
+   const hideAlert = () => {
+      alertIsVisible.value = false
+   }
+</script>
---
+++ 22-mixin-composition-1/src/components/DeleteUser.vue
@@ -0,0 +1,24 @@
+<template>
+   <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+      <p>Do you want to continue with deleting a user?</p>
+   </user-alert>
+   <section>
+      <h2>Delete a User</h2>
+      <button @click="showAlert">Delete User</button>
+   </section>
+</template>
+
+<script setup>
+   import { ref } from "vue"
+
+   import UserAlert from "./UserAlert.vue"
+
+   const alertIsVisible = ref(false)
+
+   const showAlert = () => {
+      alertIsVisible.value = true
+   }
+   const hideAlert = () => {
+      alertIsVisible.value = false
+   }
+</script>
